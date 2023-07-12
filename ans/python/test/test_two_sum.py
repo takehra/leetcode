11c33c617697b5d9bac0b31d6939074628b17734
tanaka <xiangmaozhuxibaozheng@gmail.com>
2023-07-12 18:41:00 +0900

    added parameterized test

--- ans/python/test/test_two_sum.py
+++ ans/python/test/test_two_sum.py
@@ -5,12 +5,6 @@ from p_00001_two_sum import Solution
 
 class Test_NormalCases(unittest.TestCase):
 
-    examples = [
-        {"nums": [2, 7, 11, 15], "target": 9, "ans": [0, 1]},
-        {"nums": [3, 2, 4], "target": 6, "ans": [1, 2]},
-        {"nums": [3, 3], "target": 6, "ans": [0, 1]}
-    ]
-
     def setUp(self):
         self.obj = Solution()
 
@@ -18,25 +12,20 @@ class Test_NormalCases(unittest.TestCase):
         super().tearDown()
 
     def test_solutions(self):
-        for case in self.examples:
-            for solve in [self.obj.solve_001, self.obj.solve_002, self.obj.solve_003]:
-                ret = solve(case["nums"], case["target"])
-                self.assertEqual(case["ans"], sorted(ret))
+        cases = [
+            {"nums": [2, 7, 11, 15], "target": 9, "ans": [0, 1]},
+            {"nums": [3, 2, 4], "target": 6, "ans": [1, 2]},
+            {"nums": [3, 3], "target": 6, "ans": [0, 1]}
+        ]
+        for case in cases:
+            with self.subTest(case=case):
+                for solve in [self.obj.solve_001, self.obj.solve_002, self.obj.solve_003]:
+                    ret = solve(case["nums"], case["target"])
+                    self.assertEqual(case["ans"], sorted(ret))
 
 
 class Test_ExceptionCases(unittest.TestCase):
 
-    examples = [
-        {"nums": ["2", "7", "11", "15"], "target": "9"},
-        {"nums": [2, 7, 11, 15], "target": "9"},
-        {"nums": [2], "target": 9},
-        {"nums": [-1-10**9, 7, 11, 15], "target": 9},
-        {"nums": [1+10**9, 7, 11, 15], "target": 9},
-        {"nums": [2, 7, 11, 15], "target": -1-10**9},
-        {"nums": [2, 7, 11, 15], "target": 1+10**9},
-        {"nums": "[2, 7, 11, 15]", "target": 9}
-    ]
-
     def setUp(self):
         self.obj = Solution()
 
@@ -44,15 +33,30 @@ class Test_ExceptionCases(unittest.TestCase):
         super().tearDown()
 
     def test_solutions(self):
-        for case in self.examples:
-            for solve in [self.obj.solve_001, self.obj.solve_002, self.obj.solve_003]:
-                self.assertRaises(TypeError, solve, case["nums"], case["target"])
+        cases = [
+            {"nums": ["2", "7", "11", "15"], "target": "9"},
+            {"nums": [2, 7, 11, 15], "target": "9"},
+            {"nums": [2], "target": 9},
+            {"nums": [-1-10**9, 7, 11, 15], "target": 9},
+            {"nums": [1+10**9, 7, 11, 15], "target": 9},
+            {"nums": [2, 7, 11, 15], "target": -1-10**9},
+            {"nums": [2, 7, 11, 15], "target": 1+10**9},
+            {"nums": "[2, 7, 11, 15]", "target": 9}
+        ]
+        for case in cases:
+            with self.subTest(case=case):
+                for solve in [self.obj.solve_001, self.obj.solve_002, self.obj.solve_003]:
+                    with self.assertRaises(TypeError):
+                        solve(case["nums"], case["target"])
 
 
-if __name__ == "__main__":
-
+def run_tests():
     normal_suite = unittest.TestLoader().loadTestsFromTestCase(Test_NormalCases)
     exception_suite = unittest.TestLoader().loadTestsFromTestCase(Test_ExceptionCases)
 
     all_tests = unittest.TestSuite([normal_suite, exception_suite])
     unittest.TextTestRunner().run(all_tests)
+
+
+if __name__ == "__main__":
+    run_tests()
